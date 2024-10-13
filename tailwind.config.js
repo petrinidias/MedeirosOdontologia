4c69bd476661f6733bf76b3e5e3a81364f4caf1e
petrinidias <petrini4u@gmail.com>
2024-10-13 15:25:59 -0300

    Resolvendo conflitos 2

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ['var(--font-inter)'],
+      },
+      colors: {
+        azul: "#009EFF",
+        cinza: "#929BA6",
+        butaohover: "#0383D2",
+        azulEscuro: "#0C2047",
+        azulGradiente: "#DCE4FF",
+      }
+    },
+  },
+    plugins: [],
+};
